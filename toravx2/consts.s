a49f53885936ff139a57d838831e5f84bdb25acd
Peter Schwabe <peter@cryptojedi.org>
2018-12-06 01:38:03 +0100

    Updated consts.s file to (hopefully) make OSX C compilers happy

--- toravx2/consts.s
+++ toravx2/consts.s
@@ -1,9 +1,7 @@
 	.data
 
 	.globl	mask1
-	.align 32
-	.type	mask1, @object
-	.size	mask1, 32
+	.p2align 5
 mask1:
 	.byte	1
 	.byte	1
@@ -39,9 +37,7 @@ mask1:
 	.byte	1
 
 	.globl	vrshiftsx8
-	.align 32
-	.type	vrshiftsx8, @object
-	.size	vrshiftsx8, 32
+	.p2align 5
 vrshiftsx8:
 	.long	0
 	.long	1
@@ -53,9 +49,7 @@ vrshiftsx8:
 	.long	7
 
 	.globl	maskffff
-	.align 32
-	.type	maskffff, @object
-	.size	maskffff, 32
+	.p2align 5
 maskffff:
 	.long	65535
 	.long	65535
@@ -67,9 +61,7 @@ maskffff:
 	.long	65535
 
 	.globl	maskff
-	.align 32
-	.type	maskff, @object
-	.size	maskff, 32
+	.p2align 5
 maskff:
 	.value	255
 	.value	255
@@ -89,9 +81,7 @@ maskff:
 	.value	255
 
 	.globl	q8
-	.align 32
-	.type	q8, @object
-	.size	q8, 32
+	.p2align 5
 q8:
 	.long	0
 	.long	1086849152
@@ -103,9 +93,7 @@ q8:
 	.long	1086849152
 
 	.globl	q8x
-	.align 32
-	.type	q8x, @object
-	.size	q8x, 32
+	.p2align 5
 q8x:
 	.long	12289
 	.long	12289
@@ -117,9 +105,7 @@ q8x:
 	.long	12289
 
 	.globl	v1x8
-	.align 32
-	.type	v1x8, @object
-	.size	v1x8, 32
+	.p2align 5
 v1x8:
 	.long	1
 	.long	1
@@ -131,9 +117,7 @@ v1x8:
 	.long	1
 
 	.globl	v3x8
-	.align 32
-	.type	v3x8, @object
-	.size	v3x8, 32
+	.p2align 5
 v3x8:
 	.long	3
 	.long	3
@@ -145,9 +129,7 @@ v3x8:
 	.long	3
 
 	.globl	v2730x8
-	.align 32
-	.type	v2730x8, @object
-	.size	v2730x8, 32
+	.p2align 5
 v2730x8:
 	.long	2730
 	.long	2730
@@ -159,9 +141,7 @@ v2730x8:
 	.long	2730
 
 	.globl	qinv16
-	.align 32
-	.type	qinv16, @object
-	.size	qinv16, 32
+	.p2align 5
 qinv16:
 	.long	2425857922
 	.long	1058362595
@@ -173,9 +153,7 @@ qinv16:
 	.long	1058362595
 
 	.globl	neg2
-	.align 32
-	.type	neg2, @object
-	.size	neg2, 32
+	.p2align 5
 neg2:
 	.long	0
 	.long	1072693248
@@ -187,9 +165,7 @@ neg2:
 	.long	-1074790400
 
 	.globl	neg4
-	.align 32
-	.type	neg4, @object
-	.size	neg4, 32
+	.p2align 5
 neg4:
 	.long	0
 	.long	1072693248
